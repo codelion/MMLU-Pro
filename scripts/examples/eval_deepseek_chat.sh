ea93fb2cf2530944f92bd8426355e585de91bf81
Asankhaya Sharma <codelion@users.noreply.github.com>
2025-03-06 06:47:56 +0800

    Update eval_deepseek_chat.sh

--- scripts/examples/eval_deepseek_chat.sh
+++ scripts/examples/eval_deepseek_chat.sh
@@ -3,6 +3,6 @@
 cd ../../
 
 python evaluate_from_api.py \
-                 --model_name deepseek-chat \
+                 --model_name deepseek-r1 \
                  --output_dir eval_results \
                  --assigned_subjects all
